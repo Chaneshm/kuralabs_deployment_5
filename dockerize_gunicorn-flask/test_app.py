3544992f5327abcd2a083f00391b2cb16583d236
Chanesh Mahadeo <chaneshm0812@gmail.com>
2022-11-15 14:04:00 -0500

    Fixed test

--- dockerize_gunicorn-flask/test_app.py
+++ dockerize_gunicorn-flask/test_app.py
@@ -1,10 +1,15 @@
-from application import app, greet
+from application import app
 
-def test_quick():
-  a = "jeff"
-  greeting = greet(a)
-  assert greeting == "Hi jeff "
+# def test_quick():
+#   a = "jeff"
+#   greeting = greet(a)
+#   assert greeting == "Hi jeff"
 
-# def test_home_page():
-#     response = app.test_client().get('/')
-#     assert response.status_code == 200
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    
+def testGo():
+    response = app.test_client().get('/go')
+    assert response.status_code == 302
+    assert response.location == 'http://google.com'  
